004f2e71321c80a2eae0d95a4e884f1f62a3db12
Owslebury <121823795+Owslebury@users.noreply.github.com>
2023-09-25 14:56:36 +0100

    fix EPS

--- cups/eps.py
+++ cups/eps.py
@@ -1,4 +1,3 @@
-
 import requests
 import time
 import json
@@ -7,30 +6,27 @@ import json
 api_key = 'KPENG67GT2GIT9P7'
 
 # Define the stock symbol you want to retrieve data for
-symbol = 'AAPL'  # Replace with the desired stock symbol
+symbol = 'TSLA'  # Replace with the desired stock symbol
+
+EPSvalues = []
 
 # Make API requests to fetch income statement data for the past 5 years
-for year in range(5):
-    url = f'https://www.alphavantage.co/query?function=INCOME_STATEMENT&symbol={symbol}&apikey={api_key}&datatype=csv'
-    response = requests.get(url)
+url = f'https://www.alphavantage.co/query?function=EARNINGS&symbol={symbol}&apikey={api_key}&datatype=csv'
+response = requests.get(url)
     
-    if response.status_code == 200:
+if response.status_code == 200:
         data = response.text
 
         #for i in range(0,100):
         data_dict = json.loads(data)
-        data_dict = data_dict["annualReports"]
-        for i in range(0,5):
-            print(data_dict[i]["fiscalDateEnding"])
-            print(data_dict[i]["netIncome"])
+        data_dict = data_dict["annualEarnings"]
+        for i in range(1,7):
+            #print(data_dict[i]["fiscalDateEnding"])
+            print(data_dict[i]["reportedEPS"])
+            EPSvalues.append(float(data_dict[i]["reportedEPS"]))
             
             #print(str(data[i]) + " " + str(i))
         # Extract the EPS value for the year (assuming it's in a consistent position in the CSV data)
-
-        
-    else:
-        print(f'Error fetching data for {year + 1} year(s) ago')
-
+            
     # Sleep to comply with Alpha Vantage rate limits (5 requests per minute)
-    time.sleep(12)  # Sleep for 12 seconds between requests
 
